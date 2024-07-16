9ae3123e2c46a24f7e671e9245824f9aabcbc489
Mohammad Kaif <mkaif404@gmail.com>
2024-07-16 12:14:16 +0530

    Added Do not Repeat:DRY coding technique.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:destroy,:show,:edit,:update]
+
+
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -13,12 +15,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
 
     def create
-        @article = Article.new(params.require(:article).permit(:title,:description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = 'Article was created successfully.'
             #Shorthand path for redirection is --> redirect_to @article
@@ -29,8 +30,7 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title,:description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
@@ -39,5 +39,19 @@ class ArticlesController < ApplicationController
 
     end
 
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to:'pages#about'
   get 'contact', to:'pages#contact'
-  resources :articles , only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 
 
 
